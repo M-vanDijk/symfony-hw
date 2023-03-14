a4d1597c46dc0f23b3d07c2f069c0de4c39ad5d7
M-vanDijk <94120142+M-vanDijk@users.noreply.github.com>
2023-03-14 12:28:51 +0100

    connected form to database
    added database form connection

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Tasks;
 use App\Form\TaskType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,11 +21,18 @@ class TasksController extends AbstractController
     }
 
     #[Route('/addtask', name: 'app_add_task')]
-    public function addTask(): Response
+    public function addTask(Request $request, EntityManagerInterface $entityManager): Response
     {
         $tasks = new Tasks();
 
         $form = $this->createForm(TaskType::class, $tasks);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tasks = $form->getData();
+            $entityManager->persist($tasks);
+            $entityManager->flush();
+        }
 
 
         return $this->renderForm('bezoeker/addtasks.html.twig', ['form' => $form]);
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Tasks;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +16,8 @@ class TaskType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('dueDate')
+            ->add('dueDate', DateType::class, ['label'=>'deadline'])
+            ->add('submit', SubmitType::class)
         ;
     }
 
